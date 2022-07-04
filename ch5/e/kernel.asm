ddc9a68d8f80ae8bfd91990b04a890db1a9331e5
Nowherechan <zzxy233@gmail.com>
2022-07-05 00:09:46 +0800

    desperately commit: ch5 e, I have a little understanding of InitKernel  : (

--- ch5/e/kernel.asm
+++ ch5/e/kernel.asm
@@ -9,7 +9,8 @@
 global _start	; 导出 _start
 
 _start:	; 跳到这里来的时候，我们假设 gs 指向显存
-	mov	ah, 03h				; 0000: 黑底    1111: 白字
+	mov	ah, 03h				; 0000: 黑底    1111: 白字 [03h] 青色 
 	mov	al, 'K'
 	mov	[gs:((80 * 1 + 39) * 2)], ax	; 屏幕第 1 行, 第 39 列。
 	jmp	$
+
